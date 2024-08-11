6d6e37667fdfb4d352fa7a6e108dd506d7b5166e
zhengzubing <zizbee@163.com>
2024-08-12 01:57:47 +0800

    debug opengl-demo

---
+++ opengl/build/CMakeFiles/opengl-demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl-demo.dir/opengl-demo.cpp.o"
+  "CMakeFiles/opengl-demo.dir/opengl-demo.cpp.o.d"
+  "opengl-demo"
+  "opengl-demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl-demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
